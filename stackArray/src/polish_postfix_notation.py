6a84fa93eb1ca29de6dd893036595365176482fb
hanenchemkhi <hanen.chemkhi@gmail.com>
2022-06-29 21:27:55 -0400

    Adding code to polis postfix notation module and its related test

--- stackArray/src/polish_postfix_notation.py
+++ stackArray/src/polish_postfix_notation.py
@@ -1,1 +1,23 @@
+from src.stack_list import Stack
 
+"""
+    Evaluate the postfix expression to find the answer
+
+    Args:
+       input_list(list): List containing the postfix expression
+    Returns:
+       int: Postfix expression solution
+ """
+def evaluate_postfix_expression(input_list):
+   stack = Stack()
+
+   for element in input_list :
+      if element in ["+", "-", "*", "/"] :
+         operand_1 = stack.pop()
+         operand_2 = stack.pop()
+         result = int(eval(operand_1 +element+operand_2))
+         stack.push(str(result))
+      else :
+         stack.push(element)
+   
+   return stack.pop()
