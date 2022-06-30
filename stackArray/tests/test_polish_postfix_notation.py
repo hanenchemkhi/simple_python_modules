738bfeb9640cc97000445838c28067c485cfa2e6
hanenchemkhi <hanen.chemkhi@gmail.com>
2022-06-29 21:49:13 -0400

    Modified test polish postfix notation module

--- stackArray/tests/test_polish_postfix_notation.py
+++ stackArray/tests/test_polish_postfix_notation.py
@@ -1,7 +1,12 @@
 import pytest
-from src.stack_list import Stack
+
 from src.polish_postfix_notation import evaluate_postfix_expression
 
-def test_evaluate_postfix_expression():
-    input_list = ["3", "1", "+", "4", "*"]
-    assert evaluate_postfix_expression(input) == 16
+
+@pytest.fixture
+def input_list():
+    return ["3", "1", "+", "4", "*"]
+
+def test_evaluate_postfix_expression(input_list):
+    assert evaluate_postfix_expression(input_list) == '16'
+
